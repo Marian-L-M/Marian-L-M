f685d2f3cd6c98f716fa265b129b2deb5e8d3c14
Marian-L-M <marianlmaschke@gmail.com>
2023-04-16 20:41:05 +0900

    added hamburger animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,58 @@
-const nav = document.querySelector(".nav-btns");
+// const nav = document.querySelector(".nav-btns");
 
-const menuBtn = document.querySelector(".menu-icon");
-const closeBtn = document.querySelector(".close-icon");
-const mobileNav = document.querySelector(".mobile-nav");
+// const menuBtn = document.querySelector(".menu-icon");
+// const closeBtn = document.querySelector(".close-icon");
+// const mobileNav = document.querySelector(".mobile-nav");
 
-const mobileNavLinks = document.querySelectorAll(".mobile-nav a");
+// const mobileNavLinks = document.querySelectorAll(".mobile-nav a");
 
-//Mobile Nav
-//Opening mobile menu
-menuBtn.addEventListener ("click", function() {
-    mobileNav.classList.add("active");
-    closeBtn.style.display = "block";
-    menuBtn.style.display = "none";
-})
+// //Mobile Nav
+// //Opening mobile menu
+// menuBtn.addEventListener ("click", function() {
+    //     mobileNav.classList.add("active");
+    //     closeBtn.style.display = "block";
+    //     menuBtn.style.display = "none";
+    // })
+    
+    // //Closing mobile menu
+    // closeBtn.addEventListener ("click", function() {
+        //     mobileNav.classList.remove("active");
+        //     menuBtn.style.display = "block";
+        //     closeBtn.style.display = "none";
+        // })
+        
+        //Hamburger
+window.addEventListener("load", (event) => {
+    const ham = document.querySelector(".hamburger-container");
+    ham.addEventListener ("click", function() {
+        ham.classList.toggle("active-ham");
+    })
+    
+    console.log(ham)
+  });
+// mobileNav.addEventListener ("click", function() {
+//     mobileNav.classList.remove("active");
+//     menuBtn.style.display = "block";
+//     closeBtn.style.display = "none";
+// })
 
-//Closing mobile menu
-closeBtn.addEventListener ("click", function() {
-    mobileNav.classList.remove("active");
-    menuBtn.style.display = "block";
-    closeBtn.style.display = "none";
-})
+// // Mobile Nav Menu Links
 
-mobileNav.addEventListener ("click", function() {
-    mobileNav.classList.remove("active");
-    menuBtn.style.display = "block";
-    closeBtn.style.display = "none";
-})
+// mobileNavLinks.forEach(link => (link.addEventListener("click", 
+// function(){
+//     mobileNav.classList.remove("active");
+//     menuBtn.style.display = "block";
+//     closeBtn.style.display = "none";
 
-// Mobile Nav Menu Links
+// })))
 
-mobileNavLinks.forEach(link => (link.addEventListener("click", 
-function(){
-    mobileNav.classList.remove("active");
-    menuBtn.style.display = "block";
-    closeBtn.style.display = "none";
+// // // Scroll Menu bar
 
-})))
-
-// // Scroll Menu bar
-
-window.addEventListener("scroll", () => {
-    if(window.pageYOffset > 30) {
-        nav.classList.add("scrolled");
-    } else {
-        nav.classList.remove("scrolled");
-    }
-})
+// window.addEventListener("scroll", () => {
+//     if(window.pageYOffset > 30) {
+//         nav.classList.add("scrolled");
+//     } else {
+//         nav.classList.remove("scrolled");
+//     }
+// })
 
