4d053c06316487d1b9bb0bc77a841d4406faf430
Marian-L-M <marianlmaschke@gmail.com>
2023-04-16 22:57:41 +0900

    header styles

--- components/header-component.js
+++ components/header-component.js
@@ -6,21 +6,27 @@ class Header extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
       <header>
-      <nav class="nav-btns">
-        <ul>
-          <a href="#landing"><li>INTRODUCTION</li></a>
-          <a href="#portfolio"><li>PORTFOLIO</li></a>
-          <a href="#about"><li>ABOUT</li></a>
-          <a href="#contact"><li>CONTACT</li></a>
+      <nav class="nav-menu">
+        <ul class="nav-btns">
+        <li><a href="#landing">INTRODUCTION</a></li>
+        <li><a href="#portfolio">PORTFOLIO</a></li>
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#contact">CONTACT</a></li>
         </ul>
+        </nav>
         <div class="hamburger-container" aria-label="open mobile menu">
-          <div class="hamburger">
+          <div class="hamburger" id="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
             <span></span>
             <span></span>
             <span></span>
           </div>
         </div>
-      </nav>
     </header>
     `
     }
